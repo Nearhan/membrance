10e3a418dbde8c0f03b41a9176bd63a1d0c88914
Farhan Syed <nearhan@gmail.com>
2014-11-05 14:42:24 -0500

    added basic stuff to the application

---
+++ index.js
@@ -0,0 +1,20 @@
+var neo4j = require('neo4j');
+var db = new neo4j.GraphDatabase('http://localhost:7474');
+var elasticsearch = require('elasticsearch');
+var http = require('http');
+
+
+//Main Server Logic
+server = http.createServer(function(req, res) {
+
+
+//Finish this stuff later
+});
+
+
+
+
+
+
+
+
